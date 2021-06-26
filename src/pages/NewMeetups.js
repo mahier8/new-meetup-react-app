a58cb21f309bd3458a2c296e4339e23dab69c45f
Mahier Sydow <mahier.sydow8@gmail.com>
2021-06-26 19:02:20 +0900

    form submission/props.children

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
+import NewMeetupForm from '../components/meetups/NewMeetupForm'
+
 function NewMeetupsPage() {
+    function AddMeetupHandler(meetupData) {
+        // this takes the data gathered in the newMeetupForm.js file
+        // and this is where we will use the HTTP request
+
+    }
+
     return (
-        <div>
-            New Meetups Page
-        </div>
+        <section>
+            <h1>Add new Meetup</h1>
+            <NewMeetupForm onAddMeetup={AddMeetupHandler} />
+        </section>
     )
 }
 
