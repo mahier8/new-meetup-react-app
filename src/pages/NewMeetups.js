5ddb070e7cbee2e1a82e57ed9e558ddd3f9414c7
Mahier Sydow <mahier.sydow8@gmail.com>
2021-06-27 18:58:27 +0900

    POST request/useEffect/React context

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,12 +1,29 @@
 import React from 'react'
 
+import { useHistory } from 'react-router-dom';
+
 import NewMeetupForm from '../components/meetups/NewMeetupForm'
 
 function NewMeetupsPage() {
+    const history = useHistory();
+
     function AddMeetupHandler(meetupData) {
         // this takes the data gathered in the newMeetupForm.js file
         // and this is where we will use the HTTP request
-
+        fetch('https://new-meetup-react-app-default-rtdb.firebaseio.com/meetups.json',
+        // this is what we do when we send data, to the server
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        ).then(() => {
+            history.replace('/');
+            // this is how we go back to the homepage, 
+            // after entering in our data
+        }); 
     }
 
     return (
