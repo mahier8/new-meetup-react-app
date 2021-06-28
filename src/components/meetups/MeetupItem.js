8962bd08a5a70453503da635e88aa7823cbdbd39
Mahier Sydow <mahier.sydow8@gmail.com>
2021-06-28 17:43:02 +0900

    useContext hooks

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,9 +1,28 @@
-import React from "react";
+import { useContext } from 'react';
 
+import Card from '../UI/Card';
 import classes from './MeetupItem.module.css';
-import Card from "../UI/Card";
+import FavoritesContext from '../../store/favorites-context';
 
 function MeetupItem(props) {
+  const favoritesCtx = useContext(FavoritesContext);
+
+  const itemIsFavorite = favoritesCtx.itemIsFavorite(props.id);
+
+  function toggleFavoriteStatusHandler() {
+    if (itemIsFavorite) {
+      favoritesCtx.removeFavorite(props.id);
+    } else {
+      favoritesCtx.addFavorite({
+        id: props.id,
+        title: props.title,
+        description: props.description,
+        image: props.image,
+        address: props.address,
+      });
+    }
+  }
+
   return (
     <li className={classes.item}>
       <Card>
@@ -16,7 +35,9 @@ function MeetupItem(props) {
           <p>{props.description}</p>
         </div>
         <div className={classes.actions}>
-          <button>To Favorites</button>
+          <button onClick={toggleFavoriteStatusHandler}>
+            {itemIsFavorite ? 'Remove from Favorites' : 'To Favorites'}
+          </button>
         </div>
       </Card>
     </li>
